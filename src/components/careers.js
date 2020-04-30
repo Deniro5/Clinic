77d2bda3a5818c44166397748224de6113b4a53d
USER <user@USERs-MacBook-Pro.local>
2020-04-30 01:14:48 -0400

    minor aesthetic change to job form

--- src/components/careers.js
+++ src/components/careers.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-import Grid from '@material-ui/core/Grid';
-import Imgwithtext from './Imgwithtext'
+import React, { Component } from "react";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import Imgwithtext from "./Imgwithtext";
 
 class Careers extends Component {
   constructor(props) {
@@ -10,8 +10,8 @@ class Careers extends Component {
     this.state = {
       name: "",
       resumeFileName: "",
-      cvFileName: ""
-    }
+      cvFileName: "",
+    };
   }
 
   handleSubmit(e) {
@@ -20,102 +20,110 @@ class Careers extends Component {
   }
 
   handleChange(e) {
-    this.setState({name: e.target.value});
+    this.setState({ name: e.target.value });
   }
 
   handleFileChange(event, type) {
-    console.log(type)
+    console.log(type);
     if (!event.target.files[0]) {
       return;
     }
-    if (type === "cv")
-    {
-      this.setState({ cvFileName: event.target.files[0].name })
+    if (type === "cv") {
+      this.setState({ cvFileName: event.target.files[0].name });
     } else {
-      this.setState({ resumeFileName: event.target.files[0].name })
+      this.setState({ resumeFileName: event.target.files[0].name });
     }
   }
 
   render() {
     const applitcationForm = (
-      <form onSubmit={(event) => this.handleSubmit(event)} className="application-form">
-        <div className="full-name">
+      <form onSubmit={(event) => this.handleSubmit(event)} className='application-form'>
+        <div className='full-name'>
           <TextField
-            label="First Name"
+            label='First Name'
             onChange={(event) => this.handleChange(event)}
-            className="input"
+            className='input'
             required
-            variant = {"outlined"}
-          /> 
+            variant={"outlined"}
+          />
           <TextField
-            label="Last Name"
+            label='Last Name'
             onChange={(event) => this.handleChange(event)}
-            className="input"
+            className='input'
             required
-            variant = {"outlined"}
+            variant={"outlined"}
           />
         </div>
-        <br/>
-        <TextField  
-          label="Email"
-          onChange={(event) => this.handleChange(event)}
-          className="input"
-          required
-          variant = {"outlined"}
-        /> <br/>
+        <br />
         <TextField
-          label="Phone"
+          label='Email'
           onChange={(event) => this.handleChange(event)}
-          className="input"
+          className='input'
           required
-          variant = {"outlined"}
-        /> <br/>
+          variant={"outlined"}
+        />{" "}
+        <br />
         <TextField
-          label="Position you are applying for"
+          label='Phone'
           onChange={(event) => this.handleChange(event)}
-          className="input"
+          className='input'
           required
-          variant = {"outlined"}
-        /> <br/>
+          variant={"outlined"}
+        />{" "}
+        <br />
         <TextField
-          label="Portfolio website"
+          label='Position you are applying for'
           onChange={(event) => this.handleChange(event)}
-          className="input"
+          className='input'
           required
-          variant = {"outlined"}
-        /> <br/>
-        <div className="file-container">
-          <input type="file" id="resume" onChange={(event) => this.handleFileChange(event, 'resume')} />
-          <label for="resume">Upload Your resume</label>
-          <span><p>{this.state.resumeFileName}</p></span>
+          variant={"outlined"}
+        />{" "}
+        <br />
+        <TextField
+          label='Portfolio website'
+          onChange={(event) => this.handleChange(event)}
+          className='input'
+          required
+          variant={"outlined"}
+        />{" "}
+        <br />
+        <div className='file-container'>
+          <input
+            type='file'
+            id='resume'
+            onChange={(event) => this.handleFileChange(event, "resume")}
+          />
+          <label for='resume'>Upload Your resume</label>
+          {this.state.resumeFileName.length > 0 && (
+            <p style={{ marginTop: "25px", marginLeft: "-20px" }}>
+              Resume: {this.state.resumeFileName}
+            </p>
+          )}
         </div>
-        <div className="file-container">
-          <input type="file" id="cv" onChange={(event) => this.handleFileChange(event, 'cv')} />
-          <label  for="cv">Upload Your CV</label>
-          <span><p>{this.state.cvFileName}</p></span>
-        </div>
         <TextField
-          label="Additional comments (optional)"
+          label='Additional comments (optional)'
           multiline={true}
           rows={8}
-          className="input"
-          variant = {"outlined"}
-        /> <br/>
-        <Button type="submit" color="primary" variant="contained">Submit</Button>
-    </form>
-  );
+          className='input'
+          variant={"outlined"}
+        />{" "}
+        <br />
+        <Button type='submit' color='primary' variant='contained'>
+          Submit
+        </Button>
+      </form>
+    );
 
     return (
-      <div className="job-application">
-        <Imgwithtext src = "http://www.christiancountyhd.com/file/image/m/1920/619/6470214c-20e0-405c-a63b-6008443c953b" text = ""/>
-        <Grid
-          container
-          justify="center"
-          alignItems="center"
-        >
-          <div className="container" >
+      <div className='job-application'>
+        <Imgwithtext
+          src='http://www.christiancountyhd.com/file/image/m/1920/619/6470214c-20e0-405c-a63b-6008443c953b'
+          text=''
+        />
+        <Grid container justify='center' alignItems='center'>
+          <div className='container'>
             <h1>Job Application Form</h1>
-            <div className="border"> </div>
+            <div className='border'> </div>
             <h4>A career with us means a place where your ideas are heard and valued.</h4>
             {applitcationForm}
           </div>
