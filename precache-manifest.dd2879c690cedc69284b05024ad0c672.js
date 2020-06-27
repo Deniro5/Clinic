f71aa6dd0bd730e92524f91a23e6f31908b6d969
USER <user@USERs-MacBook-Pro.local>
2020-06-26 20:27:16 -0400

    Updates

---
+++ precache-manifest.dd2879c690cedc69284b05024ad0c672.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f8b0aeea7dfb62a5bef",
+    "url": "/Clinic/static/css/main.37427ad9.chunk.css"
+  },
+  {
+    "revision": "0f8b0aeea7dfb62a5bef",
+    "url": "/Clinic/static/js/main.0f8b0aee.chunk.js"
+  },
+  {
+    "revision": "6c53e88411cdffa01421",
+    "url": "/Clinic/static/js/1.6c53e884.chunk.js"
+  },
+  {
+    "revision": "459b196f5982ef3253fe",
+    "url": "/Clinic/static/js/runtime~main.459b196f.js"
+  },
+  {
+    "revision": "0fa383e43e563435f2ebc223a6d266f7",
+    "url": "/Clinic/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Clinic/precache-manifest.eeb6652345b662f3badabb1fe7e228fc.js"
+  "/Clinic/precache-manifest.dd2879c690cedc69284b05024ad0c672.js"
 );
 
 workbox.clientsClaim();
